e03886e578bc9b6119b58dbee8d107b8a9876392
Stephen <sdarcy29@hotmail.com>
2021-10-12 20:02:57 +0000

    Add function to get the stock to try get the surplus of stock

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,6 +12,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
@@ -60,8 +62,28 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated succesfully\n")
 
+def calculate_surplus(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type
 
+    The surplus id defined as the sales figure subtracted from the stock:
+    -positive surplus indicates waste
+    -Negative surplus indicates extra made when stock was osld out
+    """
+    print("Calculating surplus data....\n")
 
-data = get_sales_data()
-sales_data =  [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data =  [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus(sales_data)
+
+print("Welcome to Love Sandwiches data automation\n")
+main()
